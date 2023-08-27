6391feb3e836ce596266ac581d17f8b73e0f8329
saeed.gholami <saeedsidjcr@gmail.com>
2023-08-27 10:36:22 +0330

    committing exercise suspend

--- app/src/main/java/com/example/asyncapptest/test1.kt
+++ app/src/main/java/com/example/asyncapptest/test1.kt
@@ -1,9 +1,11 @@
 package com.example.asyncapptest
 
 import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.delay
 
 suspend fun sus1(): List<String> {
     val myList = listOf<String>("ali", "jhon", "mamd", "jenifer","Saeed","qasem")
+    delay(3000)
     return myList.filter { it.contains("a") }
 }
 suspend fun sus2(): List<String> {
